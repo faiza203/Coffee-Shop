c6af957326f6cd5a02bad4348628f8b075868f83
faiza203 <fiazfaiza981@gmail.com>
2021-01-25 19:19:25 -0500

    Install Bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-   <h1>Home</h1>
+    <h1>Home</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
