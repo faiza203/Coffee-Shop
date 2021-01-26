cb9c5dbff4445425de366e01910cdec14758d3de
faiza203 <fiazfaiza981@gmail.com>
2021-01-26 10:13:51 -0500

    Create NavBar Component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import NavBar from "./global/navbar.js";
 import "./layout.css";
-import "./bootstrap.css"
+import "./bootstrap.css";
 
-const Layout = ({ children }) => <>{children} </>;
+const Layout = ({ children }) => (
+  <>
+    <NavBar />
+    {children}{" "}
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
