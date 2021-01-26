bb7e09f94f546e417c393c1295421e9d3e143c9b
faiza203 <fiazfaiza981@gmail.com>
2021-01-26 09:58:07 -0500

    UnIstall Bootstrap and Download bootstrap file

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 
 import "./layout.css";
+import "./bootstrap.css"
 
 const Layout = ({ children }) => <>{children} </>;
 
