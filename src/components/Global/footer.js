a6e012a0c4e14ecc0d87627dbe04899246906c22
faiza203 <fiazfaiza981@gmail.com>
2021-01-27 11:31:29 -0500

    Complete Footer

--- src/components/Global/footer.js
+++ src/components/Global/footer.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export default function footer() {
+export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="footer py-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-6 text-yellow text-center text-capitalize">
+            <h3>
+              all rights reserved &copy; {new Date().getFullYear().toString()}
+            </h3>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
