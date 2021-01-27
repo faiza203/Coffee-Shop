98e1bf44c15c13bbbf161a4b8266ee5b15f83a80
faiza203 <fiazfaiza981@gmail.com>
2021-01-27 12:59:07 -0500

    Add Info and background section in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { FaGulp } from "react-icons/fa";
+import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import BackgroundSection from "../components/Global/backgroundSection.js";
+import Info from "../components/Home/info.js";
 
-const AboutPage = () => (
+const AboutPage = ({ data }) => (
   <Layout>
-    <SEO title="About" />
-    <h1>About</h1>
+    <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="About Us"
+      styleClass="about-background"
+    />
+    <Info />
+    <FaGulp />
   </Layout>
 );
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`;
 
 export default AboutPage;
