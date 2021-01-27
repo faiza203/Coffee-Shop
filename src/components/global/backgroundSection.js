d5ce7431b5d203190f5d5c77bc433c979303ed1e
faiza203 <fiazfaiza981@gmail.com>
2021-01-27 09:57:41 -0500

    Update backgroundSection.js

--- src/components/global/backgroundSection.js
+++ src/components/global/backgroundSection.js
@@ -1,19 +1,23 @@
 import React from "react";
 import BackgroundImage from "gatsby-background-image";
 
-function BackgroundSection  ({ img, styleClass, title, children }) {
- return  <div>
-    <BackgroundImage className={styleClass} fluid={img} />
-    <h1 className="title text-white text-uppercase text-center display-4 font-weight-bold ">
-      {title}
-    </h1>
-    {children}
-  </div>
-};
+export default function BackgroundSection({
+  img,
+  styleClass,
+  title,
+  children
+}) {
+  return (
+    <BackgroundImage className={styleClass} fluid={img}>
+      <h1 className="title text-white  text-uppercase  text-center display-4 font-weight-bold">
+        {title}
+      </h1>
+      {children}
+    </BackgroundImage>
+  );
+}
 
 BackgroundSection.defaultProps = {
   title: "default title",
-  styleClass: "default-background",
+  styleClass: "default-background"
 };
-
-export default BackgroundSection;
