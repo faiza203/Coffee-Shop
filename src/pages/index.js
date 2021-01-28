614e81f805dd2e27eddfdcee2b046412fd8a0da1
faiza203 <fiazfaiza981@gmail.com>
2021-01-28 13:27:37 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export const query = graphql`
     img: file(relativePath: { eq: "default-background.jpeg" }) {
       childImageSharp {
         fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
+          ...GatsbyImageSharpFluid
         }
       }
     }
@@ -43,7 +43,7 @@ export const query = graphql`
           category
           image {
             fixed(width: 50, height: 50) {
-              ...GatsbyContentfulFixed_tracedSVG
+              ...GatsbyContentfulFixed
             }
           }
         }
