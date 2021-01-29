bbc76399d1108b0b5f8fd96f85e9140bcb5f6647
faiza203 <fiazfaiza981@gmail.com>
2021-01-29 16:18:06 -0500

    Add registration on contact us

--- src/components/Global/title.js
+++ src/components/Global/title.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function title({ title }) {
   return (
-    <div className="text-uppercase  text-center">
+    <div className="text-uppercase text-center">
       <h1>{title}</h1>
     </div>
   );
